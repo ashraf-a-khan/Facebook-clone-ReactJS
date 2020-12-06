6abf01922a9e4306640c990adcf161e019d7ee19
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-12-05 23:16:57 -0500

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBccBhhmqg1vhrqaj_Qsszn6lFjwUd_9kE",
+  authDomain: "facebook-clone-b1735.firebaseapp.com",
+  projectId: "facebook-clone-b1735",
+  storageBucket: "facebook-clone-b1735.appspot.com",
+  messagingSenderId: "469986627942",
+  appId: "1:469986627942:web:560aacbd59ab8a4350f8a7",
+  measurementId: "G-6SX0WLEJB8",
+};
